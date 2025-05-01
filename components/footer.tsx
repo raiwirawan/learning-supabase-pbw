405d8d7d33697a319e1412b0d8a308dd36001fb6
unknown <raiadi5566@gmail.com>
2025-05-01 20:42:17 +0800

    Add header and footer components into the project

---
+++ components/footer.tsx
@@ -0,0 +1,7 @@
+export default function Footer() {
+	return (
+		<div>
+			<div>THIS IS FOOTER</div>
+		</div>
+	);
+}
---
+++ components/header.tsx
@@ -0,0 +1,7 @@
+export default function Header() {
+	return (
+		<div>
+			<div>THIS IS HEADER</div>
+		</div>
+	);
+}

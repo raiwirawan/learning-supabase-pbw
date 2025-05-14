0f6f1fdbf424b73cec6e7f974eb006fb4ebb2376
Rai Wirawan <raiisyouth@gmail.com>
2025-05-14 23:47:58 +0800

    Install shadcn ui and add the button component

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,14 @@
+import { Button } from "@/components/ui/button";
+
 export default function Home() {
 	return (
 		<main className="home">
 			<div className="container mx-auto p-50">
 				<div className="w-full flex items-center justify-center">
 					THIS IS HOME PAGE
+				</div>
+				<div>
+					<Button variant="default">Click Me</Button>
 				</div>
 			</div>
 		</main>
